6fa8ddf710c1117e514ff94ccb738908f878ed15
weiwufeng <weiwufeng@sohu.com>
2016-11-19 01:47:41 +0800

    ...

--- gotcpool.go
+++ gotcpool.go
@@ -2,22 +2,24 @@ package gotcpool
 
 import (
 	"net"
+	"sync"
 )
 
 type Tcpool struct {
-	_addr *net.TCPAddr
+	_addr     *net.TCPAddr
 	_connects chan *net.TCPConn
-
-
+	_len      int
+	_created_number  int
+	_creat_mutex sync.Mutex
 }
 
-func NewTcpool(addr string) *Tcpool {
+func NewTcpool(addr string,len int) *Tcpool {
 	if a,err:=net.ResolveTCPAddr("tcp",addr);err!=nil {
 		panic(err)
 	} else {
-		p:=&Tcpool{_addr:a,_connects:make(chan *net.TCPConn,1<<10)}
+		p:=&Tcpool{_addr:a,_connects:make(chan *net.TCPConn,len),_len:len}
 
-		c:=p.NewConnect()
+		c,_:=p._CreateConnect()
 
 		p._connects<-c
 
@@ -26,7 +28,8 @@ func NewTcpool(addr string) *Tcpool {
 
 }
 
-func (p *Tcpool)NewConnect() *net.TCPConn {
+
+func (p *Tcpool)_NewConnect() *net.TCPConn {
 	if c,err:=net.DialTCP("tcp",nil,p._addr);err==nil {
 		return c
 	} else {
@@ -40,17 +43,43 @@ func (p *Tcpool)Do(f func(c *net.TCPConn)) {
 	f(c)
 }
 
-func (p *Tcpool)_Take() *net.TCPConn{
+func (p *Tcpool)PoolLen() int {
+	return len(p._connects)
+}
+
+func (p *Tcpool)_CreateConnect()  (c *net.TCPConn,all_done bool) {
+	p._creat_mutex.Lock()
+	defer p._creat_mutex.Unlock()
+	if p._created_number<100 {
+		c=p._NewConnect()
+		p._created_number++
+		return c,false
+	}
+	return nil,true
+}
+
+
+func (p *Tcpool)_Take() *net.TCPConn {
 	select {
-	case c :=<-p._connects:
+	case c:=<-p._connects:
 		return c
 	default:
-		return p.NewConnect()
+		c,all_done:=p._CreateConnect()
+		if all_done {
+			return <-p._connects
+		} else {
+			return c
+		}
 	}
 
-	return nil
+	panic("Take conntect occur a wrong")
 }
 
 func (p *Tcpool)_Revert(c *net.TCPConn) {
-	p._connects<-c
+	select {
+		case p._connects<-c:
+		default:
+			panic("Take too many connect,can't bring back the connect")
+	}
+
 }
---
+++ gotcpool_test.go
@@ -0,0 +1,80 @@
+package gotcpool
+
+
+import (
+	"github.com/blackspace/goserver"
+	"github.com/blackspace/goserver/command"
+	"github.com/blackspace/goserver/client"
+	"net"
+	"sync"
+	"testing"
+)
+
+func TestTcpool_Do(t *testing.T) {
+	command.Commands.RegistCommand("hello", func(clt *client.Client, args ...string) string {
+		return "hello,I am a robot"
+	}, "say hello")
+
+	s := goserver.NewLineServer()
+	s.Start("127.0.0.1", "5050")
+	defer s.Stop()
+
+	tcpool:=NewTcpool("127.0.0.1:5050",100)
+
+	wg:=sync.WaitGroup{}
+
+	wg.Add(1000)
+
+	for i:=0;i<1000;i++ {
+		go tcpool.Do(func(c *net.TCPConn) {
+			c.Write([]byte("hello\r\n"))
+
+			buf := make([]byte, 256)
+			_, err := c.Read(buf)
+
+			if err != nil {
+				panic(err)
+			}
+			wg.Done()
+		})
+	}
+
+	wg.Wait()
+
+	if tcpool.PoolLen()!=100 {
+		t.Fail()
+	}
+
+	tcpool.Do(func(c *net.TCPConn) {
+		c.Write([]byte("hello\r\n"))
+
+		buf := make([]byte, 256)
+		_, err := c.Read(buf)
+
+		if err != nil  {
+			panic(err)
+		}
+		if tcpool.PoolLen()!=99 {
+			t.Fail()
+		}
+	})
+
+	tcpool.Do(func(c *net.TCPConn) {
+		c.Write([]byte("hello\r\n"))
+
+		buf := make([]byte, 256)
+		_, err := c.Read(buf)
+
+		if err != nil  {
+			panic(err)
+		}
+		if tcpool.PoolLen()!=99 {
+			t.Fail()
+		}
+	})
+
+	if tcpool.PoolLen()!=100 {
+		t.Fail()
+	}
+}
+
